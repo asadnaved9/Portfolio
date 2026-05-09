f7f80b7cc8f6eabb6c7e91ce8765851d380f0764
Asad Naved <asadnaved4@gmail.com>
2026-05-09 13:28:40 +0530

    Home Page, About Page, Projects Page✅

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+/*=============== HOME SPLIT TEXT ===============*/
+const { animate, text, stagger } = anime;
+
+const { chars: chars1 } = text.split('.home__profession-1', {chars: true })
+const { chars: chars2 } = text.split('.home__profession-2', {chars: true })
+
+animate(chars1, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
+
+animate(chars2, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
+
+/*=============== SWIPER PROJECTS ===============*/
+
+const swiperProjects = new Swiper('.projects__swiper', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: 'auto',
+  grapCursor: true,
+  speed: 600,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction:false,
+  }
+});
+
+/*=============== WORK TABS ===============*/
+
+
+/*=============== SERVICES ACCORDION ===============*/
+
+
+/*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
+
+
+/*=============== COPY EMAIL IN CONTACT ===============*/
+
+
+/*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== CUSTOM CURSOR ===============*/
+
+
+/* Hide custom cursor on links */
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
