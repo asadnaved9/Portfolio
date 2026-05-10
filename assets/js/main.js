9f4a91973333cd99383b32ef0b118b146b4373ae
Asad Naved <asadnaved4@gmail.com>
2026-05-10 21:10:18 +0530

    Testimonials Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,7 +99,18 @@ servicesButtons.forEach(button => {
 })
 
 /*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
+// Duplicate images to make the animation work
 
+const tracks = document.querySelectorAll('.testimonials__content')
+
+tracks.forEach(track => {
+  const cards = [...track.children] //spread to make a static copy
+
+  //Duplicate cards only once 
+  for(const card of cards){
+    track.appendChild(card.cloneNode(true))
+  }
+})
 
 /*=============== COPY EMAIL IN CONTACT ===============*/
 
